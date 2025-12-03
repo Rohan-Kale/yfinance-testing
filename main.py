a37db12128eb3c43299fa51b8e9905ce18d19180
Rohan Kale <rohan@Rohans-MacBook-Pro.local>
2025-12-03 14:04:07 -0500

    Added subplots for diff stock charts

--- main.py
+++ main.py
@@ -6,42 +6,77 @@ import mplfinance as mpf
 import matplotlib.dates as mpdates
 import datetime as dt
 
+class GraphData:
+    
+    def __init__(self):
+        # we can also download data for multiple tickers at one by making a list ['AAPL', 'MSFT', ...]
+        self.aapl_data = yf.download("AAPL", period="3mo") 
+        self.msft_data = yf.download("MSFT", period="3mo")
+        self.nvda_data = yf.download("NVDA", period="3mo")
+        self.required_cols = ['Open','High','Low','Close','Volume']
 
-# Download AAPL only
-aapl_data = yf.download("AAPL", period="3mo") 
-msft_data = yf.download("MSFT", period="3mo")
-nvda_data = yf.download("NVDA", period="3mo")
+    
+    def filter_data(self, company):
+        if isinstance(company.columns, pd.MultiIndex):
+            company.columns = [col[0] for col in company.columns]
+        company = company.dropna(subset=self.required_cols)
+        company[self.required_cols] = company[self.required_cols].apply(pd.to_numeric, errors='coerce')
+
+        company.index = pd.to_datetime(company.index)
+        
+        return company
+
+    def plot(self):
+        self.aapl_data = self.aapl_data[self.required_cols]
+        
+        aapl_data = self.filter_data(self.aapl_data)
+        msft_data = self.filter_data(self.msft_data)
+        nvda_data = self.filter_data(self.nvda_data)
+
+        fig, axes = ppl.subplots(nrows=3, ncols=1, figsize=(12, 10), sharex=True)
+        
+        mpf.plot(
+            aapl_data,
+            type='candle',
+            ax=axes[0],
+            style='yahoo',
+            axtitle='AAPL Candlestick Chart',
+        )
+        mpf.plot(
+            msft_data,
+            type='candle',
+            ax=axes[1],
+            style='yahoo',
+            axtitle='MSFT Candlestick Chart',
+        )
+        mpf.plot(
+            nvda_data,
+            type='candle',
+            ax=axes[2],
+            style='yahoo',
+            axtitle='NVDA Candlestick Chart',
+        )
+        
+        ppl.tight_layout()
+        ppl.show()
+
+test = GraphData()
+
+test.plot()
 
 
-if isinstance(aapl_data.columns, pd.MultiIndex):
-    aapl_data.columns = [col[0] for col in aapl_data.columns]
-print("Columns:", aapl_data.columns.tolist())
 
-required_cols = ['Open','High','Low','Close','Volume']
-aapl_data = aapl_data[required_cols]
 
-aapl_data = aapl_data.dropna(subset=required_cols)
 
-aapl_data[required_cols] = aapl_data[required_cols].apply(pd.to_numeric, errors='coerce')
-aapl_data = aapl_data.dropna(subset=required_cols)
 
-aapl_data.index = pd.to_datetime(aapl_data.index)
+# ppl.plot(msft_data['Close'], label = "MSFT", color='blue')
+# ppl.plot(aapl_data['Close'], label = "AAPL", color='red')
+# ppl.plot(nvda_data['Close'], label = "NVDA", color='green')
+# ppl.xlabel("Date")
+# ppl.ylabel("Stock Price (USD)")
+# ppl.title("Stock Prices for 2025")
 
-mpf.plot(
-    aapl_data,
-    type='candle',
-    style='yahoo',
-    title='AAPL Candlestick Chart',
-)
-
-ppl.plot(msft_data['Close'], label = "MSFT", color='blue')
-ppl.plot(aapl_data['Close'], label = "AAPL", color='red')
-ppl.plot(nvda_data['Close'], label = "NVDA", color='green')
-ppl.xlabel("Date")
-ppl.ylabel("Stock Price (USD)")
-ppl.title("Stock Prices for 2025")
-
-ppl.legend()
-ppl.grid(True)
-ppl.show()
+# ppl.legend()
+# ppl.grid(True)
+# ppl.show()
 
